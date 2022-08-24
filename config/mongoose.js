d10c35144d9868c6a36b9188c086358af5193b06
krishan kant sharma <kk41110002@gmail.com>
2022-08-24 21:57:11 +0530

    install nodemons

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const ENV = require("../environment.js");
+// const ENV = require("../environment.js");
 
 main()
   .then(() => {
